e207c7b6a90c7edf57bce910b3e40b47283a7733
ibouakl <imad.bouakl@gmail.com>
2019-11-15 12:06:57 +0100

    [tracker-discovery] Bootstrap project

---
+++ tracker-config/src/test/java/fr/soat/tracker/config/TrackerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.soat.tracker.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
